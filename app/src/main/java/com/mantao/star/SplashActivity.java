bd340ee2161d36b9c4369d85145203399f75cc7c
Fathan <fathanatallahrn22@gmail.com>
2026-05-04 01:26:06 +0700

    Test

---
+++ app/src/main/java/com/mantao/star/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.mantao.star;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tampilkan splash layout
+        setContentView(R.layout.splash_screen);
+
+        // Delay 3 detik
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // biar tidak balik ke splash
+        }, 3000);
+    }
+}
